89c0fb4b748f5ee2d7a57e29d960a1dd9e03ae8c
Maxi Kucan <maximilianokucan@gmail.com>
2023-03-22 18:12:54 -0300

    Application created

---
+++ index.js
@@ -0,0 +1,84 @@
+const passwordInput = document.getElementById('password');
+const pwLength = document.getElementById("length");
+const generateButton = document.getElementById("generate");
+const copyButton = document.getElementById("copy");
+
+const options = Object.freeze({
+    numbers: "0123456789",
+    letters: "abcdefghijklmnopqrstuvwxyz",
+    special: "*¡!*+¿?|#$@-^<>¬%&="
+})
+
+const config = {
+    lowerCase: true,
+    upperCase: false,
+    numbers: false,
+    special: false
+}
+
+function generatePassword(length) {
+    let charset = "";
+    let finalPassword = [];
+
+    if (!config.upperCase && !config.lowerCase && !config.numbers && !config.special) {
+        passwordInput.value = 'No allowed characters defined.'
+        console.error('No allowed characters defined.');
+        return;
+    }
+
+    // Cargo un string con los caracteres requeridos (excediendo el largo requerido).
+    for (let i = 0; i < length; i++) {
+        if (config.lowerCase) {
+            charset += options.letters.charAt(Math.random() * options.letters.length)
+        }
+
+        if (config.upperCase){
+            charset += options.letters.charAt(Math.random() * options.letters.length).toUpperCase();
+        }
+
+        if (config.numbers) {
+            charset += options.numbers.charAt(Math.random() * options.numbers.length)
+        }
+
+        if (config.special) {
+            charset += options.special.charAt(Math.random() * options.special.length)
+        }
+
+    }
+
+    // Paso ese string a un array con el largo que pide el usuario, ordenando en un caracter requerido por cada iteración.
+    for (let i = 0; i < length; i++) {
+        finalPassword.push(charset[i]);
+
+    }
+
+    // Desordeno el array
+    finalPassword.sort(() => {
+        return 0.5 - Math.random();
+    })
+
+    // Imprimo el array final como string.
+    passwordInput.value = finalPassword.join("");
+
+}
+
+// Events
+
+generateButton.addEventListener('click', () => generatePassword(pwLength.value))
+
+copyButton.addEventListener('click', () => {
+    if (passwordInput.value.length > 0) {
+
+        navigator.clipboard.writeText(passwordInput.value);
+        alert("Password copied");
+    } else {
+        alert("Nothing to copy (¬_¬ )");
+    }
+})
+
+document.addEventListener('click', (e) => {
+    if(e.target.dataset.ref === "control"){
+        config[e.target.id] = e.target.checked;
+    }
+
+})
